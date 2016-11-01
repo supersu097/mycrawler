5675d689c3f5d0242defdbb18002164008ab13ff
susharp <glmail5051@qq.com>
2016-11-01 11:06:44 +0800

    logic refactor

--- yinwang_blog_reminder.py
+++ yinwang_blog_reminder.py
@@ -37,12 +37,17 @@ def mail_send(subject, mail_body):
 
 if __name__ == '__main__':
     while True:
-        old_url_list = [i.get('href') for i in blog_url_extract(yinwang_blog)]
-        for i in blog_url_extract(yinwang_blog):
-            if i.get('href') not in old_url_list:
-                mail_send(i.get_text(), i.get('href'))
-        else:
-            print('The blog of yinwang do not update on {},'
-                  'what the fucking sad!!!'.format(datetime.date.today()))
-        time.sleep(86400)
+        try:
+            old_url_list = [i.get('href') for i in blog_url_extract(yinwang_blog)]
+            print('The crawler is already running,just wait a second...')
+            time.sleep(86400)
+            for i in blog_url_extract(yinwang_blog):
+                if i.get('href') not in old_url_list:
+                    mail_send(i.get_text(), i.get('href'))
+            else:
+                print('The blog of yinwang do not update on {},'
+                      'what the fucking sad!!!'.format(datetime.date.today()))
+        except TypeError:
+            print('Due to the upstream unknown mistake,just keep ignoring...')
+
 
