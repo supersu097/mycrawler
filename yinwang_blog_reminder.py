4b2e155ee98b9d429041be5ece72bea8bba97689
susharp <glmail5051@qq.com>
2016-11-08 16:25:16 +0800

    code refactor

--- yinwang_blog_reminder.py
+++ yinwang_blog_reminder.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
-from bs4 import BeautifulSoup
-import requests
+
 import time
-import pprint
 import config
+import logging
 import smtplib
+import requests
+from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
-import datetime
 
 yinwang_blog = 'http://www.yinwang.org/'
 
 
 def logger_getter():
-    import logging
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s - %(levelname)s -%(message)s",
-                                    datefmt='%Y-%m-%d %H:%M')
+                                  datefmt='%Y-%m-%d %H:%M')
     file_handler = logging.FileHandler('record.log')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
@@ -29,17 +28,22 @@ def logger_getter():
     logger.addHandler(console_handler)
     return logger
 
-def blog_url_extract(url):
-    try:
-        rep_data = requests.get(url)
-        soup = BeautifulSoup(rep_data.text, 'html5lib')
-        blogs = soup.select('ul.list-group li a')
-        return blogs
-    except requests.exceptions.RequestException:
-        logger_getter().debug(' Got an issue of network that we are not very sure,'
-              'just rest a while to wait the network to normal...')
-        time.sleep(3600)
 
+def blog_source_get(url):
+    while True:
+        rep_data = requests.get(url)
+        if rep_data.status_code == 200:
+            return rep_data
+        else:
+            logger_getter().debug('Got an issue of network that we are not very sure,'
+                                  'just rest a while to wait the network to normal...')
+            time.sleep(3)
+
+
+def blog_url_extract():
+    soup = BeautifulSoup(blog_source_get(yinwang_blog).text, 'html5lib')
+    blog_url_lists = soup.select('ul.list-group li a')
+    return blog_url_lists
 
 
 def mail_send(subject, mail_body):
@@ -55,13 +59,14 @@ def mail_send(subject, mail_body):
     s.sendmail(config.sender, config.receiver, msg.as_string())
     s.quit()
 
+
 if __name__ == '__main__':
     while True:
         try:
-            old_url_list = [i.get('href') for i in blog_url_extract(yinwang_blog)]
+            old_url_list = [i.get('href') for i in blog_url_extract()]
             logger_getter().debug('The crawler is already running,just wait a second...')
             time.sleep(86400)
-            for i in blog_url_extract(yinwang_blog):
+            for i in blog_url_extract():
                 if i.get('href') not in old_url_list:
                     mail_send(i.get_text(), i.get('href'))
             else:
@@ -69,5 +74,3 @@ if __name__ == '__main__':
                                       'what the fucking sad!!!')
         except TypeError:
             logger_getter().debug('Due to the upstream unknown mistake,just keep ignoring...')
-
-
