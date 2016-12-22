168113eb28383422cccdfc636011717edbb408f9
susharp <glmail5051@qq.com>
2016-12-23 01:44:34 +0800

    refactor code a lot
    solve some issue in the special scenario  which I didn't previously consider

--- yinwang_blog_reminder.py
+++ yinwang_blog_reminder.py
@@ -57,7 +57,8 @@ def blog_source_get():
             return rep_data
         else:
             logger_getter().debug("It seems that U don't get the "
-                                  "blog source yet,just quit...")
+                                  "blog source yet,and it had pulled one in"
+                                  " local filesystem then just quit...")
             blog_source_write(rep_data)
             exit(0)
     except requests.exceptions.RequestException:
@@ -72,52 +73,60 @@ def blog_aTag_extract(blog_source_upstream):
     return blog_aTag_lists
 
 
+def berfore_mail_send_check(diff_blog, blog_list_status):
+    if len(diff_blog) == 1:
+        logger_getter().debug('Yinwang {0} a blog...'.format(blog_list_status))
+        mail_send('Yinwang {0} a blog: '.format(blog_list_status) +
+                  [_.get_text().strip() for _ in diff_blog][0],
+                  [_.get('href') for _ in diff_blog][0])
+    else:
+        logger_getter().debug(
+            'Yinwang {0} more than one blogs...'.format(blog_list_status))
+        mail_send('Yinwang {0} more than one blogs'.format(blog_list_status),
+                  '\n'.join(
+                      [_.get_text().strip() + ':' + _.get('href')
+                       for _ in diff_blog]))
+
+    # There is a fucking thing,it should be very very careful to use the
+    # functionality of reformating code in Pycharm since you comment some
+    # code,coz I found Pycharm will change the the position of the origin
+    # code line you commented to a place you don't expect generally its
+    # parent code block but for the plugin of Python-mode in vim seem to no
+    # longer exist such problem,nice~
+
+    # write new blog source to the local file
+    blog_source_write(blog_source_get())
+
 if __name__ == '__main__':
-    new_aTag_list = blog_aTag_extract(blog_source_get())
-    old_aTag_list = blog_aTag_extract(open('blog_source.html'))
-
-    if len(new_aTag_list) < len(old_aTag_list):
-        disappeared_blog = set(old_aTag_list) - set(new_aTag_list)
-        if len(disappeared_blog) == 1:
-            logger_getter().debug('Yinwang deleted a blog...')
-            mail_send('垠神删除了一篇博客: '.decode('utf-8') +
-                      [_.get_text() for _ in disappeared_blog][0],
-                      [_.get('href') for _ in disappeared_blog][0])
-        else:
-            logger_getter().debug('Yinwang deleted more than one blog...')
-            mail_send('垠神删除了不止一篇博客'.decode('utf-8'),
-                      '\n'.join(
-                          [_.get_text() + ':' + _.get('href')
-                           for _ in disappeared_blog]))
-        # write new blog source to the local file
-        if True:
-            blog_source_write(blog_source_get())
-
-    elif len(new_aTag_list) > len(old_aTag_list):
-        new_blog = set(new_aTag_list) - set(old_aTag_list)
-        if len(new_blog) == 1:
-            logger_getter().debug('Yinwang published a new blog...')
-            mail_send('垠神发表了一篇新博客: '.decode('utf-8') +
-                      [_.get_text() for _ in new_blog][0],
-                      [_.get('href') for _ in new_blog][0])
-        else:
-            logger_getter().debug('Yinwang published more than one new blog...')
-            mail_send(
-                '垠神发表了不止一篇新博客'.decode('utf-8'),
-                '\n'.join(
-                    [_.get_text() + ': ' + _.get('href') for _ in new_blog]))
-
-        # There is a fucking thing,it should be very very careful to use the
-        # functionality of reformating code in Pycharm since you comment some
-        # code,coz I found Pycharm will change the the position of the origin
-        # code line you commented to a place you don't expect generally its
-        # parent code block but for the plugin of Python-mode in vim seem to no
-        # longer exist such problem,nice~
-
-        # write new blog source to the local file
-        if True:
-            blog_source_write(blog_source_get())
-
-    elif len(new_aTag_list) == len(old_aTag_list):
+    uniq_new_aTag_list = set(blog_aTag_extract(blog_source_get()))
+    uniq_old_aTag_list = set(blog_aTag_extract(open('blog_source.html')))
+    if uniq_old_aTag_list == uniq_new_aTag_list:
         logger_getter().debug('Yinwang do not have a new '
                               'blog to be published yet!')
+    else:
+        # In this situation, Yinwang deletes and publish same amount of blogs at
+        # the same time.
+        if len(uniq_old_aTag_list) == len(uniq_new_aTag_list):
+            logger_getter().debug('Yinwang deletes and publish same amount of '
+                                  'blogs at the same time')
+            new_blog = uniq_new_aTag_list - uniq_old_aTag_list
+            disappeared_blog = (
+                uniq_new_aTag_list ^ uniq_old_aTag_list) - new_blog
+            berfore_mail_send_check(new_blog, 'published')
+            berfore_mail_send_check(disappeared_blog, 'deleted')
+        else:
+            if len(uniq_new_aTag_list) < len(uniq_old_aTag_list):
+                disappeared_blog = uniq_old_aTag_list - uniq_new_aTag_list
+                new_blog = (
+                    uniq_old_aTag_list ^ uniq_new_aTag_list) - disappeared_blog
+                berfore_mail_send_check(disappeared_blog, 'deleted')
+                if len(new_blog) != 0:
+                    berfore_mail_send_check(new_blog, 'published')
+
+            elif len(uniq_new_aTag_list) > len(uniq_old_aTag_list):
+                new_blog = uniq_new_aTag_list - uniq_old_aTag_list
+                disappeared_blog = (
+                    uniq_new_aTag_list ^ uniq_old_aTag_list) - new_blog
+                berfore_mail_send_check(new_blog, 'published')
+                if len(disappeared_blog) != 0:
+                    berfore_mail_send_check(disappeared_blog, 'deleted')
