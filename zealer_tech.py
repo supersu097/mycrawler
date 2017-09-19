6f49b4a435e9def461b2700c8b6710f769a43260
sharp <sharp.gan1993@gmail.com>
2017-09-19 18:36:26 +0800

    refactor a lot for going back to the essence of the usage scenario

--- zealer_tech.py
+++ zealer_tech.py
@@ -2,32 +2,44 @@
 # coding=utf-8
 
 import os
-from core import html
+
+from bs4 import BeautifulSoup
+
 from core import helper
+from core import html
 
 zealer_tech = 'http://www.zealer.com:8080/list?cp=2'
 page_source=html.page_source_get(zealer_tech, pagetype='special')
-first_aTag = html.first_a_tag_extract(page_source, 'li.series_item a')
-
-video_href =first_aTag.get('href')
-video_url = 'http://www.zealer.com:8080' + video_href
-video_title = first_aTag.text.split(' ')[-1]
+soup = BeautifulSoup(page_source, 'html5lib')
+videos_aTags = soup.select('li.series_item a')
 temp_data= helper.TEMP_DIR + '/zealer_tech.txt'
 
+
+def data_persistence():
+    with open(temp_data, 'w') as f:
+        for i in videos_aTags:
+            f.write(i.get('href') + '\n')
+
+
 def check_new():
-    if not os.path.isfile(temp_data):
-        html.first_url_persistence(video_href, '/zealer_tech.txt')
-        helper.logger_getter().info("First init to store the url of the first video!")
-        exit(0)
+
     with open(temp_data) as f:
-        # if new first url doesn't equal to the record one, upgrade it first!
-        if video_href != f.readline():
-            helper.logger_getter().info('Zealer published a new video!')
-            helper.logger_getter().info('Renew the first url in the file')
-            html.first_url_persistence(video_href, '/zealer_tech.txt')
-            helper.mail_send('Zealer发了新视频: ' + video_title, video_url)
-        else:
-            helper.logger_getter().info('Zealer did not publish any new video yet!')
+        old_hrefs_list = [i.split('')[0] for i in f.readlines()]
+
+    new_videos = [i for i in videos_aTags if i.get('href') not in old_hrefs_list]
+    if len(new_videos) == 0:
+        helper.logger_getter().info('Zealer did not publish any new video yet!')
+    else:
+        helper.mail_send(helper.date_getter() + '  Zealer发了新视频')
+        helper.logger_getter().info('Zealer published new video!')
+        data_persistence()
+        helper.logger_getter().info("Renew the videos' href in the file")
+
+
 
 if __name__ == '__main__':
+    if not os.path.isfile(temp_data):
+        data_persistence()
+        helper.logger_getter().info("First init to store all videos' href!")
+        exit(0)
     check_new()
