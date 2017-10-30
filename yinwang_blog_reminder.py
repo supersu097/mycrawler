fd6c83df298911751a8896d4e2d278e1de5ebea4
sharp <sharp.gan1993@gmail.com>
2017-10-30 22:48:15 +0800

    bugfix of dumplicate mail

--- yinwang_blog_reminder.py
+++ yinwang_blog_reminder.py
@@ -22,11 +22,11 @@ def check_new(option):
 
     with open(temp_file) as f:
         previous_posts = [i.split('\n')[0] for i in f.readlines()]
+    # inside the list of new_posts, there still are the aTag object
     new_posts = [i for i in aTags_list if i.get('href') + '|' + i.get_text() not in previous_posts]
 
     if len(new_posts) == 0:
         helper.logger_getter().info('Yin did not publish any blog yet!')
-        return
 
     else:
         for i in new_posts:
@@ -35,16 +35,16 @@ def check_new(option):
             blog_title = i.get_text()
             helper.logger_getter().info(msg_content)
             helper.mail_send(helper.date_getter() + '  ' + msg_content + ':' + blog_title, blog_url)
-            helper.dir_check(helper.CURR_PATH + '/yinblog_back')
-            html.make_screenshot(blog_url, helper.CURR_PATH + '/yinblog_back/' +  blog_title + '.png')
+            # helper.dir_check(helper.CURR_PATH + '/yinblog_back')
+            # html.make_screenshot(blog_url, helper.CURR_PATH + '/yinblog_back/' + blog_title + '.png')
         data_persistence()
 
     # decide whether push the screenshot to github repo or not
-    if option:
-        os.system('git add .')
-        os.system("git commit -m 'auto backup yinwang"
-                  " blog - {}'".format(helper.date_getter()))
-        os.system('git push origin master')
+    # if option:
+    #     os.system('git add .')
+    #     os.system("git commit -m 'auto backup yinwang"
+    #               " blog - {}'".format(helper.date_getter()))
+    #     os.system('git push origin master')
 
 
 if __name__ == '__main__':
