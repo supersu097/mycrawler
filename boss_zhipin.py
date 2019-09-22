d7c78cc8fb6b7b9147e5c908420f68f7e3df9c66
sharp <sharp.gan1993@gmail.com>
2019-09-22 21:07:28 +0800

    update

--- boss_zhipin.py
+++ boss_zhipin.py
@@ -1,54 +1,63 @@
 # coding = 'utf-8'
-from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
-from user_agent import generate_user_agent
-from os.path import join as os_join
+
+import requests
 from bs4 import BeautifulSoup
-import os
+from user_agent import generate_user_agent
 import time
 
 
-class Lagou(object):
-    CURR_PATH = os.path.abspath('.')
-    CHROME_DRIVER_PATH: str = os_join(CURR_PATH, 'chromedriver_mac')
+class BossZhipin(object):
+    def __init__(self, url, page_num):
+        self.url = url
+        self.page_num = page_num
 
-    def __init__(self, initial_url: str):
-        from selenium.webdriver.chrome.options import Options as ChromeOptions
-        self.initial_url: str = initial_url
-        options: ChromeOptions = ChromeOptions()
-        # options.add_argument('headless')
-        custom_ua: str = generate_user_agent(os='win')
-        options.add_argument('user-agent=' + custom_ua)
-        self.driver = ChromeDriver(executable_path=self.CHROME_DRIVER_PATH,
-                                   chrome_options=options)
+    def get_page_source(self):
+        common_header = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+                                   "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                         "accept-encoding": "gzip, deflate, br",
+                         "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+                         "user-agent": generate_user_agent(os='win')}
+        cookie = {'Cookie': "_uab_collina=156859572282842185693821; __g=-; __c=1568554446;"
+                            " _bl_uid=nkkv20m5lbg0z6tmdhq0neb2eL4b; __l=r=https%3A%2F%2Fsao."
+                            "zhipin.com%2F&l=%2Fwww.zhipin.com%2Fweb%2Fgeek%2Frecommend&"
+                            "friend_source=0&friend_source=0; lastCity=101280600; _uab_collina"
+                            "=156914858939269034789901; __zp_stoken__=5c57D15xzkhQxhenoVfQUqnpa"
+                            "QyZqJEqTtkZY5iuQsqpEMobrIseo%2F8vzHjNlFY7Lm8nKbM8eHyH3zyskvBjFPMy8g%3D%3D;"
+                            " __a=75666192.1569148589..1568554446.5.1.5.5; t=ihzxNowfNoQFYPs; wt=ihzxNowfNoQFYPs"}
 
-    def get_first_page_source(self):
-        try:
-            self.driver.implicitly_wait(5)
-            self.driver.set_script_timeout(20)
-            self.driver.set_page_load_timeout(25)
-            self.driver.get(self.initial_url)
-            page_source: str = self.driver.page_source
-            return page_source
-        except KeyboardInterrupt:
-            self.driver.quit()
-            print('The exception of KeyboardInterrupt '
-                  'detected and the chrome driver has quited.')
-        except Exception as e:
-            print(str(e))
-            self.driver.quit()
-            exit(1)
+        if self.page_num == 1:
+            common_header['referer'] = 'https://www.zhipin.com/c101280600/?' \
+                                       'query=linux%E8%BF%90%E7%BB%B4&page=2&ka=page-2'
+        else:
+            common_header['referer'] = "https://www.zhipin.com/c101280600/?' \
+                                       'query=linux%E8%BF%90%E7%BB%B4&page={0}&" \
+                                       "ka=page-{0}".format(str(page_num - 1))
+        response = requests.get(self.url, headers=common_header, cookies=cookie)
+        response.encoding = 'utf-8'
+        content = response.text
+        return content
 
-
-def page_parser():
-    soup = BeautifulSoup(get_page_source(), 'lxml')
-    item_list = soup.select("div.job-list ul li")
-    for item in item_list:
-        job_title = item.select_one("div.job-title").get_text()
-        company_name = item.select_one("h3.name a").get_text().strip()
-        job_salary = item.select_one("span.red").get_text()
-        print({'job_title': job_title,
-               'company_name': company_name,
-               'job_salary': job_salary})
+    def page_parser(self):
+        soup = BeautifulSoup(self.get_page_source(), 'lxml')
+        item_list = soup.select("div.job-list ul li")
+        for item in item_list:
+            job_title = item.select_one("div.job-title").get_text()
+            company_name = item.select_one("h3.name a").get_text().strip()
+            job_salary = item.select_one("span.red").get_text()
+            print({'job_title': job_title,
+                   'company_name': company_name,
+                   'job_salary': job_salary})
 
 
-page_parser()
+for page_num in range(1, 1000):
+    print('Get page ' + str(page_num))
+    url = "https://www.zhipin.com/c101280600/?query=linux" \
+          "%E8%BF%90%E7%BB%B4&page={0}&ka=page-{0}".format(str(page_num))
+    boss = BossZhipin(url, page_num)
+    boss.page_parser()
+    current_page_source = boss.get_page_source()
+    current_page_soup = BeautifulSoup(current_page_source, 'lxml')
+    if_next_page = current_page_soup.select_one('a.next.disabled')
+    if if_next_page:
+        break
+    time.sleep(8)
